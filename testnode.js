7a4231f28cf4144e69094bebe654761608ddd553
bambaYK <138519630+bambaYK@users.noreply.github.com>
2025-01-24 14:26:48 -0500

    Update1 testnode.js
    creation d'express, axios, d'app et d'une requete post pour le traitement de la requete envoyer depuis php

--- testnode.js
+++ testnode.js
@@ -1,1 +1,9 @@
+cont express = require('express');
+cont axios = require('axios');
+cont app = express();
 
+app.use(express.json());
+
+app.post('/node', async (req, res) => {
+  console.log('Recu depuis php', req.body)
+  
