b466f6c1fe887c8397822d3134c04eac6839e34b
bambaYK <138519630+bambaYK@users.noreply.github.com>
2025-01-24 14:38:08 -0500

    Update2 testnode.js
    Envoie des donnees de python a php et renvoyer des donnees de php a python

--- testnode.js
+++ testnode.js
@@ -7,3 +7,16 @@ app.use(express.json());
 app.post('/node', async (req, res) => {
   console.log('Recu depuis php', req.body)
   
+// Envoie des donnees a python 
+  try{
+    const response  = await axios.post('http://localhost/5000/python', req.body);
+    console.log('reponse de Python', response.data)
+    res.json(response.data); // Renvoyer la reponse a php  
+  } catch (err){
+    console.log('Erreur avec python', err.message);
+    res.status(500).send('Erreur avec le service python')
+  }
+});
+
+app.listen(3000, () => console.log('Ecoute sur le port 3000'));
+  
