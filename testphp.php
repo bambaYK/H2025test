4f47797b62cd3a92142d79de50cd9109b1f7ecfa
bambaYK <bambaproca@gmail.com>
2025-01-26 19:05:22 -0500

    update2 testphp
    Envoie des donnees, Configuration de la requete POST et du contexte, Affichage de la reponse de Node.js

--- testphp.php
+++ testphp.php
@@ -4,20 +4,21 @@ $data = array(
     "description" => "demande de test de communication"
 );
 
+$options = array(
+    'http' => array(
+        'header' => "Content-Type: application/json\r\n",
+        'method' => "POST",
+        'content' => json_encode($data)
+    )
+    );
 
+$context = stream_context_create($options);
+$response = file_get_contents('http://localhost:3000/node', false, $context);
 
+if ($response === FALSE){
+    die('Erreur avec la requete Node.js');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+echo "Reponse de Node.js : " . $response;
 
 ?>
